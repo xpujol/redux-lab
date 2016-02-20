6d76360eb5949dd1244946adeff84c01a04bc700
Carles Pagès <carles.pages.rozas@gmail.com>
2016-02-20 17:40:59 +0100

    Use next monday as default start day

--- src/State/StartDay.js
+++ src/State/StartDay.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 const CHANGE_START_DAY = 'StartDay/Change';
 
 export function ChangeStartDay(date)
@@ -5,7 +7,9 @@ export function ChangeStartDay(date)
   return {type : CHANGE_START_DAY, date};
 }
 
-const reducer = function(state = new Date(), action)
+const nextMonday = moment().day(8);
+
+const reducer = function(state = nextMonday, action)
 {
   switch(action.type){
     case CHANGE_START_DAY:
